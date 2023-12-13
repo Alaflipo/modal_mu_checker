f87b701888f57a1d29c390104f563b9ef9598352
Morris Boers <morrisboers@gmail.com>
2023-12-13 01:07:32 +0100

    Completed mcf parsing and added command line input tool

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from LTS import LTS
 from modal_mu_formula import ModalMuFormula
+import argparse
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-lts", "--lts", help="File path to the labeled transtion system file (in .aut format)")
+argParser.add_argument("-mcf", "--mcf", help="File path to the mu calculus formula (in .mcf format)")
 
 def main(): 
-    lts = LTS('./testcases/modal_operators/test.aut')
+    # folders = ['boolean', 'combined', 'fixpoints_only', 'modal_operators']
+    # for folder in folders: 
+    #     files = [file for file in os.listdir('./testcases/{}'.format(folder)) if file[-3:] == 'mcf']
+    #     for file in files: 
+    #         filepath = './testcases/{folder}/{file}'.format(folder=folder, file=file)
+    #         with open(filepath) as f: 
+    #             print("\n" + f.read().strip())
+    #         formula = ModalMuFormula(filepath)
+    #         print(formula)
+    args = argParser.parse_args()
+    lts = LTS(args.lts)
     print(lts)
-    
-    for i in range(9): 
-        formula = ModalMuFormula('./testcases/boolean/form{form_number}.mcf'.format(form_number = (i + 1)))
-        print(formula)
+    formula = ModalMuFormula(args.mcf)
+    print(formula)
 
 if __name__ == "__main__": 
     main() 
--- modal_mu_formula.py
+++ modal_mu_formula.py
@@ -28,6 +28,14 @@ class RecursionVar:
     
     def __str__(self): 
         return self.variable_name
+    
+class ActionVar: 
+
+    def __init__(self, action_name: str): 
+        self.action_name: str = action_name
+    
+    def __str__(self): 
+        return self.action_name
 
 class Operator: 
 
@@ -45,8 +53,46 @@ class LogicFormula:
         self.rhs = rhs  
     
     def __str__(self): 
-        return '{lhs} {opp} {rhs}'.format(lhs=self.lhs, opp=self.opp, rhs=self.rhs)
-        
+        return '({lhs} {opp} {rhs})'.format(lhs=self.lhs, opp=self.opp, rhs=self.rhs)
+
+class MuFormula: 
+
+    def __init__(self, recursion_var, formula): 
+        self.recursion_var = recursion_var
+        self.formula = formula 
+
+    def __str__(self): 
+        return 'mu {variable}. {formula}'.format(variable=self.recursion_var, formula=self.formula)     
+
+class NuFormula: 
+
+    def __init__(self, recursion_var, formula):
+        self.recursion_var = recursion_var
+        self.formula = formula 
+
+    def __str__(self): 
+        return 'nu {variable}. {formula}'.format(variable=self.recursion_var, formula=self.formula)     
+
+class DiamondFormula: 
+    
+    def __init__(self, action, formula):
+        self.action = action
+        self.formula = formula 
+
+    def __str__(self): 
+        return '<{action}>{formula}'.format(action=self.action, formula=self.formula)     
+
+class BoxFormula: 
+    
+    def __init__(self, action, formula):
+        self.action = action
+        self.formula = formula 
+
+    def __str__(self): 
+        return '[{action}]{formula}'.format(action=self.action, formula=self.formula)     
+
+
+
 class ModalMuFormula: 
 
     def __init__(self, filepath): 
@@ -58,7 +104,7 @@ class ModalMuFormula:
         start = self.counter 
         end = start + len(string_to_expect)
         if self.formula_string[start: end] != string_to_expect:
-            print("error")
+            raise Exception("Did not find expected: {}".format(string_to_expect))
         else: 
             self.counter += len(string_to_expect)
     
@@ -66,18 +112,30 @@ class ModalMuFormula:
         while self.counter < len(self.formula_string) and self.formula_string[self.counter] == ' ' : 
             self.counter += 1
     
-    def parse_literal(self, boolean, string_bool): 
+    def required_white_space(self): 
+        if self.formula_string[self.counter] != ' ': 
+            raise Exception("White space required!")
+        self.skip_white_space()
+    
+    def parse_literal(self, boolean, string_bool) -> Literal: 
         self.expect(string_bool)
         self.skip_white_space()
         return Literal(boolean)
 
-    def parse_recursion_var(self): 
+    def parse_recursion_var(self) -> RecursionVar: 
         recursion_var = RecursionVar(self.formula_string[self.counter])
         self.counter += 1 
         self.skip_white_space()
         return recursion_var
     
-    def parse_operator(self): 
+    def parse_action_name(self) -> ActionVar:
+        action = '' 
+        while self.counter < len(self.formula_string) and self.formula_string[self.counter] in action_name_first: 
+            action += self.formula_string[self.counter]
+            self.counter += 1
+        return ActionVar(action)
+    
+    def parse_operator(self) -> Operator: 
         if (self.formula_string[self.counter] == and_opp_first): 
             self.expect("&&")
             self.skip_white_space()
@@ -87,28 +145,50 @@ class ModalMuFormula:
             self.skip_white_space()
             return Operator("||") 
 
-    def parse_logic_formula(self): 
+    def parse_logic_formula(self) -> LogicFormula: 
         self.expect("(")
         self.skip_white_space()
         rhs, lhs, opp = None, None, None 
         if (self.formula_string[self.counter] in formula_first):
             lhs = self.parse_formula()
-        else: print('error')
+        else: raise Exception("Left hand side in logic formula missing!")
         if (self.formula_string[self.counter] in opp_first): 
             opp = self.parse_operator()
-        else: print('error')
+        else: raise Exception("Operator in logic formula missing!")
         if (self.formula_string[self.counter] in formula_first): 
             rhs = self.parse_formula()
-        else: print('error')
+        else: raise Exception("Right hand side in logic formula missing!")
         self.expect(")")
         self.skip_white_space() 
         return LogicFormula(lhs, opp, rhs)
     
-    def parse_mu_formula(self): 
-        pass 
+    def parse_mu_nu_formula(self, u_type) -> MuFormula | NuFormula: 
+        self.expect(u_type)
+        self.required_white_space() 
+        recursion_var, formula = None, None  
+        if self.formula_string[self.counter] in recursion_var_first: 
+            recursion_var = self.parse_recursion_var()
+        else: raise Exception("No recursion variable found in {} formula".format(u_type))
+        self.expect(".")
+        self.skip_white_space()
+        if self.formula_string[self.counter] in formula_first: 
+            formula = self.parse_formula()
+        else: raise Exception("No formula found in {} formula".format(u_type))
+        return MuFormula(recursion_var, formula) if u_type == "mu" else NuFormula(recursion_var, formula)
 
-    def parse_nu_formula(self): 
-        pass 
+    def parse_box_diamond_formula(self, expected: list[str]) -> DiamondFormula | BoxFormula: 
+        self.expect(expected[0])
+        self.skip_white_space()
+        action, formula = None, None 
+        if (self.formula_string[self.counter] in action_name_first): 
+            action = self.parse_action_name()
+        else: raise Exception("No action variable in {} found!".format(expected[0] + expected[1]))
+        self.expect(expected[1])
+        self.skip_white_space()
+        if (self.formula_string[self.counter] in formula_first): 
+            formula = self.parse_formula()
+        else: raise Exception("No formula after {} found".format(expected[0] + expected[1]))
+        return DiamondFormula(action, formula) if expected[0] == "<" else BoxFormula(action, formula)
 
     def read_and_parse_formula(self, filepath): 
         with open(filepath) as file: 
@@ -128,8 +208,18 @@ class ModalMuFormula:
         # Check if we have found the start of a logic formula 
         elif (self.formula_string[self.counter] == logic_first): 
             return self.parse_logic_formula()
+        # Check if we have found the start of a mu fixed point formula
         elif (self.formula_string[self.counter] == mu_first): 
-            return self.parse_mu_formula() 
+            return self.parse_mu_nu_formula("mu") 
+        # Check if we have found the start of a nu fixed point formula
+        elif (self.formula_string[self.counter] == nu_first): 
+            return self.parse_mu_nu_formula("nu") 
+        # Check if we have found the start of a diamond modality formula
+        elif (self.formula_string[self.counter] == diamond_first): 
+            return self.parse_box_diamond_formula(['<', '>'])
+        # Check if we have found the start of a box modality formula
+        elif (self.formula_string[self.counter] == box_first): 
+            return self.parse_box_diamond_formula(['[', ']'])
             
     def __str__(self): 
         return '{formula}'.format(formula=self.formula)
